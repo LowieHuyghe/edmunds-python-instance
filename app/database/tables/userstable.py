5d88db5c8716cf4372d419512589e8e4e6a401ce
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2017-10-21 15:44:15 +0200

    #16: Issue #478 of flask-sqlalchemy seems to be fixed.

--- app/database/tables/userstable.py
+++ app/database/tables/userstable.py
@@ -6,6 +6,5 @@ UsersTable = Table('users',
                    Column('id', Integer, primary_key=True),
                    Column('name', String(50)),
 
-                   extend_existing=True,  # To dodge: https://github.com/mitsuhiko/flask-sqlalchemy/issues/478
                    # info={'bind_key': 'users_database'},
                    )
