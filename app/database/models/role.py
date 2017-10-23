75bcb4dc414305664aeebbad79b9056467af789c
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2017-10-23 22:27:07 +0200

    #16: Updated role and user after updated model.

--- app/database/models/role.py
+++ app/database/models/role.py
@@ -1,6 +1,4 @@
 
-from edmunds.database.model import mapper
-from edmunds.auth.tables.roletable import RoleTable
 from edmunds.auth.models.role import Role as EdmundsRole
 
 
@@ -8,20 +6,5 @@ class Role(EdmundsRole):
     """
     Role Model
     """
-    __table__ = RoleTable
 
-    def __init__(self, id, name=None, description=None):
-        """
-        Constructor
-        :param id:          The id
-        :param name:        The name
-        :param description: The description
-        """
-        super(Role, self).__init__(
-            id,
-            name=name,
-            description=description
-        )
-
-
-mapper(Role, RoleTable)
+    pass
---
+++ app/database/models/userroles.py
@@ -0,0 +1,5 @@
+
+from edmunds.auth.models.userroles import UserRolesTable as EdmundsUserRolesTable
+
+
+UserRolesTable = EdmundsUserRolesTable
